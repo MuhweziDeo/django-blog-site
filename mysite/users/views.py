f0c24f33c0dae94c4260daeb5bd3c7cce4a44e86
muhwezideo <aggrey256@gmail.com>
2018-10-14 12:11:47 +0200

    add login and logout functionality

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username=form.cleaned_data.get('username')
             messages.success(request,'Account created for {}'.format(username))
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form=UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
