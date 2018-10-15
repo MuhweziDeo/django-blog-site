eaed956c2a9559b8339b39bfe92062c275c43858
muhwezideo <aggrey256@gmail.com>
2018-10-15 16:07:12 +0200

    class based view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 # Create your views here.
 posts=Post.objects.all()
 def home(request):
@@ -11,4 +12,5 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html',{'title':'About'})
-  
+class PostListView(ListView):
+    model=Post
