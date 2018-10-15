47b4d9da28111622f3f2232cac69c695fc82ca30
muhwezideo <aggrey256@gmail.com>
2018-10-15 15:39:22 +0200

    fix save error for user registration

---
+++ mysite/users/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+class UserRegisterForm (UserCreationForm):
+    email=forms.EmailField()
+
+    class Meta:
+        # handles configuration
+        # model to which data will be stored
+        model=User
+        # fields on the form
+        fields=['username', 'email','password1','password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email=forms.EmailField()
+
+    class Meta:
+        model=User
+        fields=['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields=['image']
+     
