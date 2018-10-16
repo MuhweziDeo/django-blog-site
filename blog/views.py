969258d9322592e0e7b88bc3b16b0e5ca1811284
muhwezideo <aggrey256@gmail.com>
2018-10-16 09:58:47 +0200

    add update routes for posts

---
+++ blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
+# Create your views here.
+posts=Post.objects.all()
+# def home(request):
+#     context = {
+#         'posts': posts
+#     }
+#     return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html',{'title':'About'})
+
+class PostListView(ListView):
+    model=Post
+    template_name='blog/home.html'
+    context_object_name='posts'
+    ordering=['date_posted']
+
+class PostDetailView(DetailView):
+    model=Post
+
+class PostCreateView(CreateView):
+    model=Post
+    fields=['title','content']
+
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(UpdateView):
+    model=Post
+    fields=['title','content']
+
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
